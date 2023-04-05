4c05ff977752dd68947cfc567490634e779dec26
Junior-Oliveira <franciscoanto@gmail.com>
2023-04-05 00:00:10 -0300

    :truck: Rotas pelo inertia
    Add as rotas via inertia para uso do spa

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -75,42 +75,26 @@ const logout = () => {
           <v-app-bar-nav-icon></v-app-bar-nav-icon>
             </template>
 
-            <v-app-bar-title>Espindola Admin</v-app-bar-title>
+            <v-app-bar-title>              
+              <Link :href="route('dashboard')">
+                Espindola Admin
+              </Link>
+            </v-app-bar-title>
 
             <v-spacer></v-spacer>
 
             <v-btn icon="far fa-bell"></v-btn>
-            <v-btn icon="far fa-circle-user"></v-btn>
+            <v-btn>
+              <Link :href="route('profile.show')">
+                <v-icon icon="far fa-circle-user"></v-icon>
+              </Link>
+            </v-btn>
 
           </v-app-bar>
 
             <v-row>
               <v-col v-for="card in cards" :key="card" cols="12">
-                <v-card>
-                  <v-list lines="two">
-                    <v-list-subheader>{{ card }}</v-list-subheader>
-                    <template v-for="n in 6" :key="n">
-                      <v-list-item>
-                        <template v-slot:prepend>
-                          <v-avatar color="grey-darken-1"></v-avatar>
-                        </template>
-
-                        <v-list-item-title>Message {{ n }}</v-list-item-title>
-
-                        <v-list-item-subtitle>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Nihil repellendus distinctio similique
-                        </v-list-item-subtitle>
-                      </v-list-item>
-
-                      <v-divider
-                        v-if="n !== 6"
-                        :key="`divider-${n}`"
-                        inset
-                      ></v-divider>
-                    </template>
-                  </v-list>
-                </v-card>
+                <slot />
               </v-col>
             </v-row>
           </v-container>
