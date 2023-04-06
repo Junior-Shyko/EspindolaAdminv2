54d65fcdb47d21359ba0d3f10e3991e869370cfc
Junior-Oliveira <franciscoanto@gmail.com>
2023-04-06 00:27:24 -0300

    :lipstick: Estilizando
    Add novas classes e removido outras do css para estilizar o content

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -32,68 +32,113 @@ const logout = () => {
 </script>
 
 <template>
-  <div>
+  <div class="">
     <Head :title="title" />
 
     <Banner />
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen">
       <v-app id="inspire">
-       
         <v-navigation-drawer v-model="drawer">
-          <v-sheet color="grey-lighten-4" class="pa-4">
+          <!-- <v-sheet color="grey-lighten-4" class="pa-4">
             <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
 
             <div>john@google.com</div>
-          </v-sheet>
-
+          </v-sheet> -->
+          <v-card class="mx-auto" color="grey-lighten-4">
+            <v-avatar
+              class="mb-4"
+              color="grey-darken-1"
+              size="64"
+              style="margin-left: 100px; margin-top: 10px"
+            >
+            </v-avatar>
+            <v-card-title class="flex-just-center">
+              {{ $page.props.auth.user.name }}
+            </v-card-title>
+          </v-card>
           <v-divider></v-divider>
 
-          <v-list>
-            <v-list-item v-for="[icon, text] in links" :key="icon" link>
-              <template v-slot:prepend>
-                <v-icon>{{ icon }}</v-icon>
+          <v-list v-model:opened="open">
+            <v-list-item prepend-icon="fas fa-home" title="Home"></v-list-item>
+
+            <v-list-group value="Users">
+              <template v-slot:activator="{ props }">
+                <v-list-item
+                  v-bind="props"
+                  prepend-icon="fas fa-circle-user"
+                  title="Usuarios"
+                  style="font-style: 10px"
+                ></v-list-item>
               </template>
 
-              <v-list-item-title>{{ text }}</v-list-item-title>
-            </v-list-item>
+              <v-list-group value="Admin">
+                <template v-slot:activator="{ props }">
+                  <v-list-item
+                    v-bind="props"
+                    prepend-icon="fas fa-table-columns"
+                    title="Admin"
+                    style="font-style: 10px"
+                  ></v-list-item>
+                </template>
+
+                <v-list-item
+                  v-for="([title, icon], i) in admins"
+                  :key="i"
+                  :title="title"
+                  :prepend-icon="icon"
+                  :value="title"
+                ></v-list-item>
+              </v-list-group>
+
+              <v-list-group value="Actions">
+                <template v-slot:activator="{ props }">
+                  <v-list-item v-bind="props" title="Imóveis"></v-list-item>
+                </template>
+
+                <v-list-item
+                  v-for="([title, icon], i) in cruds"
+                  :key="i"
+                  :value="title"
+                  :title="title"
+                  :prepend-icon="icon"
+                ></v-list-item>
+              </v-list-group>
+            </v-list-group>
           </v-list>
         </v-navigation-drawer>
 
-        <v-main>
+        <v-main class="bg-slate-100 dark:bg-gray-900">
           <v-container class="py-8 px-6" fluid>
             <v-app-bar
-        color="teal-darken-4"
-        image="https://picsum.photos/1920/1080?random"
-      >
-        <template v-slot:image>
-          <v-img
-            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-          ></v-img>
-        </template>
+              color="teal-darken-4"
+              image="https://picsum.photos/1920/1080?random"
+            >
+              <template v-slot:image>
+                <v-img
+                  gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+                ></v-img>
+              </template>
 
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-            </template>
+              <template v-slot:prepend>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+              </template>
 
-            <v-app-bar-title>              
-              <Link :href="route('dashboard')">
-                Espindola Admin
-              </Link>
-            </v-app-bar-title>
+              <v-app-bar-title>
+                <Link :href="route('dashboard')"> Espindola Admin </Link>
+              </v-app-bar-title>
 
-            <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
 
-            <v-btn icon="far fa-bell"></v-btn>
-            <v-btn>
-              <Link :href="route('profile.show')">
-                <v-icon icon="far fa-circle-user"></v-icon>
-              </Link>
-            </v-btn>
-
-          </v-app-bar>
+              <v-btn icon="far fa-bell"></v-btn>
+              <v-btn>
+                <Link :href="route('profile.show')">
+                  <v-icon icon="far fa-circle-user"></v-icon>
+                </Link>
+              </v-btn>
+            </v-app-bar>
 
             <v-row>
-              <v-col v-for="card in cards" :key="card" cols="12">
+              <v-col>
                 <slot />
               </v-col>
             </v-row>
@@ -127,13 +172,24 @@ export default {
         value: "buzz",
       },
     ],
-    cards: ['Today', 'Yesterday'],
+    cards: ["Today", "Yesterday"],
     links: [
-        ['mdi-inbox-arrow-down', '  '],
-        ['mdi-send', 'Send'],
-        ['mdi-delete', 'Trash'],
-        ['mdi-alert-octagon', 'Spam'],
-      ],
+      ["mdi-inbox-arrow-down", "  "],
+      ["mdi-send", "Lucas"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+    open: ["Users"],
+    admins: [
+      ["Gerenciar", "fas fa-user-pen"],
+      ["Config", "fas fa-gear"],
+    ],
+    cruds: [
+      ["Imóveis", "fas fa-home"],
+      ["Add Imoveis", "fas fa-plus"],
+      ["Chave", "fas fa-key"],
+      ["Vistoria", "fas fa-pen-to-square"],
+    ],
   }),
 
   watch: {
@@ -153,5 +209,12 @@ export default {
 }
 .area-desktop-xs {
   margin-left: 0px;
+}
+.flex-just-center {
+  display: flex;
+  justify-content: center;
+}
+.v-list-item--density-default.v-list-item--one-line {
+  min-height: 5px !important;
 }
 </style>
