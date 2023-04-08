7e15186a6cc1bdbf52f63c40e0f48b730d4a5fd4
Junior-Oliveira <franciscoanto@gmail.com>
2023-04-08 18:37:16 -0300

    #9 - Alterado nome do campo de tabela

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactSiteController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ContactSiteController::class)->group(function () {
+    Route::get('/todos-contatos', 'all')->name('site.contact.all');
 });
