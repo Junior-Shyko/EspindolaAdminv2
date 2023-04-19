a47a76b9e3c2eb4fab2f1e25d1bc3cfdcb9619d2
Junior-Oliveira <franciscoanto@gmail.com>
2023-04-19 19:36:37 -0300

    :safety_vest: #9 - Add validação no Backend
    Add validação nas classes de StoreContactSiteRequest

--- app/Http/Requests/StoreContactSiteRequest.php
+++ app/Http/Requests/StoreContactSiteRequest.php
@@ -3,8 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Validation\ValidationException;
 
 class StoreContactSiteRequest extends FormRequest
 {
@@ -13,7 +11,7 @@ class StoreContactSiteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,28 +22,7 @@ class StoreContactSiteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'cep' => 'required|max:25',
-            'address' => 'required|max:256',
-            'email' => 'email|required|max:256',
-            'creci' => 'required|max:25',
-            'cnpj' => 'required|max:50'
+            //
         ];
     }
-
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array<string, string>
-     */
-    public function messages(): array
-    {
-        return [
-            'cep.required' => 'O CEP é obrigatório',
-            'address.required' => 'O Logradouro é obrigatório',
-            'email.required' => 'O E-mail é obrigatório',
-            'creci.required' => 'O CRECI é obrigatório',
-            'cnpj.required' => 'O CNPJ é obrigatório'   
-        ];
-    }
-
 }
